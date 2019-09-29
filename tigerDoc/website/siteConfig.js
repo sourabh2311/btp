6844e9bd77ade719014d312b093f77bfefcb452e
sourabh2311 <sourabhaggarwal231196@gmail.com>
2019-09-29 11:11:02 +0530

    Added tiger documentation demo

--- tigerDoc/website/siteConfig.js
+++ tigerDoc/website/siteConfig.js
@@ -22,7 +22,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "Tiger Compiler",
+  projectName: "TigerCompiler",
   organizationName: "Sourabh Aggarwal",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
